4e1207c2d0cb3be9a7471e771a9198a5955f66e2
kjakx <k.jak.xg0g11@gmail.com>
2022-06-09 14:33:41 +0000

    added test for Tokenizer::new()

--- jack_compiler/src/main.rs
+++ jack_compiler/src/main.rs
@@ -7,10 +7,17 @@ fn main() {
 #[cfg(test)]
 mod tests {
     #[test]
-    fn test_new_tokenizer() {
+    fn test_tokenizer_new() {
         use super::tokenizer;
         use std::fs::File;
-        let t = tokenizer::Tokenizer::new(File::open("/workspace/Jack-compiler/jack_compiler/jack/ExpressionLessSquare/Main.jack").unwrap());
-        println!("tokens: {:?}", t.tokens);
+        let t_els = tokenizer::Tokenizer::new(File::open("/workspace/Jack-compiler/jack_compiler/jack/ExpressionLessSquare/Main.jack").unwrap());
+        println!("tokens in ExpressionLessSquare/Main.jack:");
+        println!("{:?}", t_els.tokens);
+        let t_s = tokenizer::Tokenizer::new(File::open("/workspace/Jack-compiler/jack_compiler/jack/Square/Main.jack").unwrap());
+        println!("tokens in Square/Main.jack:");
+        println!("{:?}", t_s.tokens);
+        let t_at = tokenizer::Tokenizer::new(File::open("/workspace/Jack-compiler/jack_compiler/jack/ArrayTest/Main.jack").unwrap());
+        println!("tokens in ArrayTest/Main.jack:");
+        println!("{:?}", t_at.tokens);
     }
 }
