8f9613b5ca1ea2a7a4e8cf96941904002ce331c1
kjakx <k.jak.xg0g11@gmail.com>
2022-06-09 14:10:39 +0000

    test Tokenizer::new()

--- jack_compiler/src/main.rs
+++ jack_compiler/src/main.rs
@@ -1,3 +1,16 @@
+mod tokenizer;
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_new_tokenizer() {
+        use super::tokenizer;
+        use std::fs::File;
+        let t = tokenizer::Tokenizer::new(File::open("/workspace/Jack-compiler/jack_compiler/jack/ExpressionLessSquare/Main.jack").unwrap());
+        println!("tokens: {:?}", t.tokens);
+    }
 }
