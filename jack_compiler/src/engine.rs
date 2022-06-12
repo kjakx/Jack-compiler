35a5a86537f0d898d9e3774f90e284de6446c7b4
kjakx <k.jak.xg0g11@gmail.com>
2022-06-12 16:47:54 +0000

    compile_subroutine, compile_parameterList

--- jack_compiler/src/engine.rs
+++ jack_compiler/src/engine.rs
@@ -9,6 +9,9 @@ pub struct Engine {
 
 impl Engine {
     pub fn new(t: Tokenizer, f: File) -> Self {
+        if t.token_type() == Token::Empty {
+            t.advance();
+        }
         Engine {
             tokenizer: t,
             writer: BufWriter::<File>::new(f),
@@ -18,7 +21,6 @@ impl Engine {
     pub fn compile_class(&mut self) {
         writeln!(self.writer, "<class>").unwrap();
         // "class"
-        self.tokenizer.advance();
         match self.tokenizer.token_type() {
             Token::Keyword(class) => {
                 writeln!(self.writer, "<keyword> {} </keyword>", class_name).unwrap();
@@ -48,9 +50,29 @@ impl Engine {
             }
         }
         // classVarDec*
-        self.compile_class_var_dec();
+        'classVarDec: loop {
+            match self.tokenizer.peek_next_token() {
+                &Token::Keyword(&attribute @ "static" | "field") => {
+                    self.tokenizer.advance();
+                    self.compile_class_var_dec();
+                },
+                _ => {
+                    break 'classVarDec;
+                }
+            }
+        }
         // subroutineDec*
-        self.compile_subroutine();
+        'subroutineDec: loop {
+            match self.tokenizer.peek_next_token() {
+                &Token::Keyword(&attribute @ "constructor" | "function" | "method") => {
+                    self.tokenizer.advance();
+                    self.compile_subroutine();
+                },
+                _ => {
+                    break 'subroutineDec;
+                }
+            }
+        }
         // '}'
         self.tokenizer.advance();
         match self.tokenizer.token_type() {
@@ -68,16 +90,9 @@ impl Engine {
     pub fn compile_class_var_dec(&mut self) {
         writeln!(self.writer, "<classVarDec>").unwrap();
         // 'static' | 'field'
-        self.tokenizer.advance();
         match self.tokenizer.token_type() {
             Token::Keyword(&attribute @ "static" | "field") => {
                 writeln!(self.writer, "<keyword> {} </keyword>", attribute).unwrap();
-                /*
-                    s => {
-                        panic!("'static' or 'field' expected, found {}", s);
-                    }
-                }
-                */
             },
             t => {
                 panic!("'static' or 'field' expected, found {}", t);
@@ -88,16 +103,6 @@ impl Engine {
         match self.tokenizer.token_type() {
             Token::Keyword(&type_name @ "int" | "char" | "boolean") => {
                 writeln!(self.writer, "<keyword> {} </keyword>", type_name).unwrap();
-                /*
-                match type_name.as_str() => {
-                    'int' | 'char' | 'boolean' => {
-                        writeln!(self.writer, "<keyword> {} </keyword>", type_name).unwrap();
-                    },
-                    s => {
-                        panic!("'int', 'char', or 'boolean' expected, found {}", s);
-                    }
-                }
-                */
             },
             Token::Identifier(class_name) => {
                 writeln!(self.writer, "<identifier> {} </identifier>", class_name).unwrap();
@@ -119,15 +124,16 @@ impl Engine {
                 }
             }
             // ','
-            self.tokenizer.advance();
-            match self.tokenizer.token_type() {
-                Token::Symbol(comma @ ',') => {
+            match self.tokenizer.peek_next_token() {
+                &Token::Symbol(comma @ ',') => {
+                    self.tokenizer.advance();
                     writeln!(self.writer, "<symbol> {} </symbol>", comma).unwrap();
                 },
                 _ => { break 'varName; }
             }
         }
         // ';'
+        self.tokenizer.advance();
         match self.tokenizer.token_type() {
             Token::Symbol(semicolon @ ';') => {
                 writeln!(self.writer, "<symbol> {} </symbol>", semicolon).unwrap();
@@ -142,37 +148,117 @@ impl Engine {
     
     pub fn compile_subroutine(&mut self) {
         writeln!(self.writer, "<subroutineDec>").unwrap();
-
-
+        // 'constructor' | 'function' | 'method'
+        self.tokenizer.advance();
+        match self.tokenizer.token_type() {
+            Token::Keyword(&attribute @ "constructor" | "function" | "method") => {
+                writeln!(self.writer, "<keyword> {} </keyword>", attribute).unwrap();
+            },
+            t => {
+                panic!("'constructor', 'function' or 'method' expected, found {}", t);
+            }
+        }
+        // 'void' | type
+        self.tokenizer.advance();
+        match self.tokenizer.token_type() {
+            Token::Keyword(&void @ "void") => {
+                writeln!(self.writer, "<keyword> {} </keyword>", void).unwrap();
+            },
+            Token::Keyword(&type_name @ "int" | "char" | "boolean") => {
+                writeln!(self.writer, "<keyword> {} </keyword>", type_name).unwrap();
+            },
+            Token::Identifier(class_name) => {
+                writeln!(self.writer, "<identifier> {} </identifier>", class_name).unwrap();
+            },
+            t => {
+                panic!("'void' or type expected, found {}", t);
+            }
+        }
+        // subroutineName
+        self.tokenizer.advance();
+        match self.tokenizer.token_type() {
+            Token::Identifier(subroutine_name) => {
+                writeln!(self.writer, "<identifier> {} </identifier>", subroutine_name).unwrap();
+            },
+            t => {
+                panic!("subroutineName expected, found {}", t);
+            }
+        }
+        // '('
+        self.tokenizer.advance();
+        match self.tokenizer.token_type() {
+            Token::Symbol(left_parenthesis @ '(') => {
+                writeln!(self.writer, "<symbol> {} </symbol>", left_parenthesis).unwrap();
+            },
+            t => {
+                panic!("'(' expected, found {}", t);
+            }
+        }
+        // parameterList
+        self.compile_parameter_list();
+        // ')'
+        self.tokenizer.advance();
+        match self.tokenizer.token_type() {
+            Token::Symbol(left_parenthesis @ ')') => {
+                writeln!(self.writer, "<symbol> {} </symbol>", left_parenthesis).unwrap();
+            },
+            t => {
+                panic!("')' expected, found {}", t);
+            }
+        }
+        // subroutineBody
+        writeln!(self.writer, "<subroutineBody>").unwrap();
+        // '{'
+        self.tokenizer.advance();
+        match self.tokenizer.token_type() {
+            Token::Symbol(left_brace @ '{') => {
+                writeln!(self.writer, "<symbol> {} </symbol>", left_brace).unwrap();
+            },
+            t => {
+                panic!("'{{' expected, found {}", t);
+            }
+        }
+        // varDec*
+        'varDec: loop {
+            match self.tokenizer.peek_next_token() {
+                Token::Keyword(var @ 'var') => {
+                    self.compile_var_dec();
+                },
+                _ => { break 'varDec; }
+            }
+        }
+        // statements
+        self.compile_statements();
+        // '}'
+        self.tokenizer.advance();
+        match self.tokenizer.token_type() {
+            Token::Symbol(right_brace @ '}') => {
+                writeln!(self.writer, "<symbol> {} </symbol>", right_brace).unwrap();
+            },
+            t => {
+                panic!("'}}' expected, found {}", t);
+            }
+        }
+        writeln!(self.writer, "</subroutineBody>").unwrap();
+        writeln!(self.writer, "</subroutineDec>").unwrap();
     }
+
     pub fn compile_parameter_list(&mut self) { // this method looks ahead a ')' symbol token.
         writeln!(self.writer, "<parameterList>").unwrap();
         // (type varName (',' type varName)*)?
         'parameterList: loop {
-            // type?
-            self.tokenizer.advance();
-            match self.tokenizer.token_type() {
-                Token::Keyword(&type_name @ "int" | "char" | "boolean") => {
+            // type
+            match self.tokenizer.peek_next_token() {
+                &Token::Keyword(&type_name @ "int" | "char" | "boolean") => {
+                    self.tokenizer.advance();
                     writeln!(self.writer, "<keyword> {} </keyword>", type_name).unwrap();
-                    /*
-                    match type_name.as_str() => {
-                        'int' | 'char' | 'boolean' => {
-                            writeln!(self.writer, "<keyword> {} </keyword>", type_name).unwrap();
-                        },
-                        s => {
-                            panic!("'int', 'char', or 'boolean' expected, found {}", s);
-                        }
-                    }
-                    */
                 },
-                Token::Identifier(class_name) => {
+                &Token::Identifier(class_name) => {
+                    self.tokenizer.advance();
                     writeln!(self.writer, "<identifier> {} </identifier>", class_name).unwrap();
                 },
-                Token::Symbol(right_parenthesis @ ')') => {
+                _ => {
                     break 'parameterList;
-                }
-                t => {
-                    panic!("type or ')' expected, found {}", t);
                 }
             }
             // varName
@@ -186,16 +272,13 @@ impl Engine {
                 }
             }
             // ','
-            self.tokenizer.advance();
-            match self.tokenizer.token_type() {
-                Token::Symbol(comma @ ',') => {
+            match self.tokenizer.peek_next_token() {
+                &Token::Symbol(comma @ ',') => {
+                    self.tokenizer.advance();
                     writeln!(self.writer, "<symbol> {} </symbol>", comma).unwrap();
                 },
-                Token::Symbol(right_parenthesis @ ')') => {
+                _ => {
                     break 'parameterList;
-                }
-                t => {
-                    panic!("type or ')' expected, found {}", t);
                 }
             }
         }
@@ -256,9 +339,9 @@ impl Engine {
                 }
             }
             // ','
-            self.tokenizer.advance();
-            match self.tokenizer.token_type() {
-                Token::Symbol(comma @ ',') => {
+            match self.tokenizer.peek_next_token() {
+                &Token::Symbol(comma @ ',') => {
+                    self.tokenizer.advance();
                     writeln!(self.writer, "<symbol> {} </symbol>", comma).unwrap();
                 },
                 _ => { break 'varName; }
@@ -275,6 +358,7 @@ impl Engine {
         }
         writeln!(self.writer, "</varDec>").unwrap();
     }
+
     pub fn compile_statements(&mut self) { unimplemented!(); }
     pub fn compile_do(&mut self) { unimplemented!(); }
     pub fn compile_let(&mut self) { unimplemented!(); }
@@ -283,5 +367,5 @@ impl Engine {
     pub fn compile_if(&mut self) { unimplemented!(); }
     pub fn compile_expression(&mut self) { unimplemented!(); }
     pub fn compile_term(&mut self) { unimplemented!(); }
-    pub fn compile_expression_list(&mut self) { unimplemented!(); }
+    pub fn compile_expression_lis          t(&mut self) { unimplemented!(); }
 }
