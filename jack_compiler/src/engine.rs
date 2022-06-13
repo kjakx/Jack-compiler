2e9ebfda67f0270ba75ac9741b4f0df2c5582642
kjakx <k.jak.xg0g11@gmail.com>
2022-06-13 14:02:01 +0000

    refactor compile_while

--- jack_compiler/src/engine.rs
+++ jack_compiler/src/engine.rs
@@ -395,55 +395,15 @@ impl Engine {
 
     pub fn compile_while(&mut self) {
         writeln!(self.writer, "<whileStatement>").unwrap();
-        // 'while'
-        match self.tokenizer.peek_next_token() {
-            &Token::Keyword(_ @ "while") {
-                self.compile_keyword();
-            },
-            t => {
-                panic!("'while' expected, found {}", t);
-            }
-        }
-        // '('
-        match self.tokenizer.peek_next_token() {
-            &Token::Symbol(_ @ '(') {
-                self.compile_symbol();
-            },
-            t => {
-                panic!("'(' expected, found {}");
-            }
-        }
-        // expression
+        // 'while' '(' expression ')'
+        self.compile_keyword_expect("while")
+        self.compile_symbol_expect('(');
         self.compile_expression();
-        // ')'
-        match self.tokenizer.peek_next_token() {
-            &Token::Symbol(_ @ ')') {
-                self.compile_symbol();
-            },
-            t => {
-                panic!("')' expected, found {}");
-            }
-        }
-        // '{'
-        match self.tokenizer.peek_next_token() {
-            &Token::Symbol(_ @ '{') {
-                self.compile_symbol();
-            },
-            t => {
-                panic!("'{' expected, found {}");
-            }
-        }
-        // statements
+        self.compile_symbol_expect(')');
+        // '{' statements '}'
+        self.compile_symbol_expect('{');
         self.compile_statements();
-        // '}'
-        match self.tokenizer.peek_next_token() {
-            &Token::Symbol(_ @ '}') {
-                self.compile_symbol();
-            },
-            t => {
-                panic!("'}' expected, found {}");
-            }
-        }
+        self.compile_symbol_expect('}');
         writeln!(self.writer, "</whileStatement>").unwrap();
     }
 
