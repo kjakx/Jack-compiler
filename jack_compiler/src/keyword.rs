7c7dbe6f28b531e7183799791cd593ffb05d8854
kjakx <k.jak.xg0g11@gmail.com>
2022-06-14 14:34:15 +0000

    from_str

--- jack_compiler/src/keyword.rs
+++ jack_compiler/src/keyword.rs
@@ -1,6 +1,16 @@
 use std::fmt;
+use std::str::FromStr;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Clone)]
+pub struct BadKeywordError;
+
+impl fmt::Display for BadKeywordError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Bad keyword")
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub enum Keyword {
     Class,
     Method,
@@ -54,30 +64,32 @@ impl fmt::Display for Keyword {
 }
 
 impl FromStr for Keyword {
+    type Err = BadKeywordError;
+
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let kw = match s {
-            "class" => Ok(Keyword::Class),
-            "method" => Ok(Keyword::Method),
-            "function" => Ok(Keyword::Function),
+        match s {
+            "class"       => Ok(Keyword::Class),
+            "method"      => Ok(Keyword::Method),
+            "function"    => Ok(Keyword::Function),
             "constructor" => Ok(Keyword::Constructor),
-            "int" => Ok(Keyword::Int),
-            "boolean" => Ok(Keyword::Boolean),
-            "char" => Ok(Keyword::Char),
-            "void" => Ok(Keyword::Void),
-            "var" => Ok(Keyword::Var),
-            "static" => Ok(Keyword::Static),
-            "field" => Ok(Keyword::Field),
-            "let" => Ok(Keyword::Let),
-            "do" => Ok(Keyword::Do),
-            "if" => Ok(Keyword::If),
-            "else" => Ok(Keyword::Else),
-            "while" => Ok(Keyword::While),
-            "return" => Ok(Keyword::Return),
-            "true" => Ok(Keyword::True),
-            "false" => Ok(Keyword::False),
-            "null" => Ok(Keyword::Null),
-            "this" => Ok(Keyword::This),
-            _ => Err("bad keyword");
+            "int"         => Ok(Keyword::Int),
+            "boolean"     => Ok(Keyword::Boolean),
+            "char"        => Ok(Keyword::Char),
+            "void"        => Ok(Keyword::Void),
+            "var"         => Ok(Keyword::Var),
+            "static"      => Ok(Keyword::Static),
+            "field"       => Ok(Keyword::Field),
+            "let"         => Ok(Keyword::Let),
+            "do"          => Ok(Keyword::Do),
+            "if"          => Ok(Keyword::If),
+            "else"        => Ok(Keyword::Else),
+            "while"       => Ok(Keyword::While),
+            "return"      => Ok(Keyword::Return),
+            "true"        => Ok(Keyword::True),
+            "false"       => Ok(Keyword::False),
+            "null"        => Ok(Keyword::Null),
+            "this"        => Ok(Keyword::This),
+            _             => Err(BadKeywordError),
         }
     }
 }
