bfcc2182422b8a3675ac97823657dbbfd661137b
kjakx <k.jak.xg0g11@gmail.com>
2022-06-15 13:27:55 +0000

    source as dir, passed test

--- jack_compiler/src/analyzer.rs
+++ jack_compiler/src/analyzer.rs
@@ -3,39 +3,65 @@ use std::fs::File;
 use crate::tokenizer::*;
 use crate::engine::*;
 
-pub struct Analyzer {
-    engine: Engine,
-}
+pub struct Analyzer;
 
 impl Analyzer {
-    pub fn new(source: &Path) -> Self {
-        let fin = File::open(source).expect("cannot open source file");
-        let t = Tokenizer::new(fin);
-        let fout = File::create(source.with_extension("xml")).expect("cannot create output file");
-        Analyzer {
-            engine: Engine::new(t, fout)
+    pub fn run(source: &Path) {
+        if source.is_dir() {
+            for f in source.read_dir().expect("read_dir call failed") {
+                if let Ok(f) = f {
+                    if f.path().extension().unwrap() == "jack" {
+                        let fin = File::open(f.path()).expect("cannot create source file");
+                        let fout = File::create(f.path().with_extension("xml")).expect("cannot create output file");
+                        let t = Tokenizer::new(fin);
+                        let mut e = Engine::new(t, fout);
+                        e.compile();
+                    }
+                }
+            }
+        } else {
+            let fin = File::open(source).expect("cannot create source file");
+            let fout = File::create(source.with_extension("xml")).expect("cannot create output file");
+            let t = Tokenizer::new(fin);
+            let mut e = Engine::new(t, fout);
+            e.compile();
         }
-    }
-
-    pub fn run(&mut self) {
-        self.engine.compile();
     }
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn test_analyzer() {
+    fn test_analyze_from_file() {
         use super::*;
         use std::path::Path;
         use std::process::Command;
 
         let source = Path::new("/workspace/Jack-compiler/jack_compiler/jack/Square/Main.jack");
-        let mut a = Analyzer::new(source);
-        a.run();
+        Analyzer::run(source);
         let fout = source.with_extension("xml").to_string_lossy().into_owned();
         let forg = source.with_extension("xml.org").to_string_lossy().into_owned();
         let diff_status = Command::new("diff").args(["-b", "-u", "-w", &fout, &forg]).status().expect("failed to execute process");
         assert!(diff_status.success());
+    }
+
+    #[test]
+    fn test_analyze_from_dir() {
+        use super::*;
+        use std::path::Path;
+        use std::process::Command;
+
+        let source_dir = Path::new("/workspace/Jack-compiler/jack_compiler/jack/Square/");
+        for f in source_dir.read_dir().expect("read_dir call failed") {
+            if let Ok(f) = f {
+                if f.path().extension().unwrap() == "jack" {
+                    Analyzer::run(&f.path());
+                    let fout = f.path().with_extension("xml").to_string_lossy().into_owned();
+                    let forg = f.path().with_extension("xml.org").to_string_lossy().into_owned();
+                    let diff_status = Command::new("diff").args(["-b", "-u", "-w", &fout, &forg]).status().expect("failed to execute process");
+                    assert!(diff_status.success());
+                }
+            }
+        }
     }
 }
