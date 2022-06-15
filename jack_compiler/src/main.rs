718705e197475d823e347c46ed300263092189a8
kjakx <k.jak.xg0g11@gmail.com>
2022-06-15 14:02:15 +0000

    parser

--- jack_compiler/src/main.rs
+++ jack_compiler/src/main.rs
@@ -1,5 +1,15 @@
 mod tokenizer;
+mod engine;
+mod keyword;
+mod symbol;
+mod analyzer;
+
+use std::env;
+use std::path::Path;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 { panic!("usage: jackc <filename>.jack | <dirname>"); }
+    let arg_path = Path::new(&args[1]);
+    analyzer::Analyzer::run(arg_path);
 }
