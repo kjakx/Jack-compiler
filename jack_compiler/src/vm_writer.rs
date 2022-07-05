3e3cf55c00b38249cf25dc82122ef7a59f394fce
kjakx <k.jak.xg0g11@gmail.com>
2022-07-05 01:36:40 +0000

    VMWriter module

---
+++ jack_compiler/src/vm_writer.rs
@@ -0,0 +1,139 @@
+use std::io::{BufWriter, Write};
+use std::fs::File;
+use std::fmt;
+
+pub enum Segment {
+    Const,
+    Arg,
+    Local,
+    Static,
+    This,
+    That,
+    Pointer,
+    Temp,
+}
+
+impl fmt::Display for Segment {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Segment::Const => {
+                write!(f, "const")
+            },
+            Segment::Arg => {
+                write!(f, "argument")
+            },
+            Segment::Local => {
+                write!(f, "local")
+            },
+            Segment::Static => {
+                write!(f, "static")
+            },
+            Segment::This => {
+                write!(f, "this")
+            },
+            Segment::That => {
+                write!(f, "that")
+            },
+            Segment::Pointer => {
+                write!(f, "pointer")
+            },
+            Segment::Temp => {
+                write!(f, "temp")
+            },
+        }
+    }
+}
+
+pub enum Command {
+    Add,
+    Sub,
+    Neg,
+    Eq,
+    Gt,
+    Lt,
+    And,
+    Or,
+    Not,
+}
+
+impl fmt::Display for Command {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Command::Add => {
+                write!(f, "add")
+            },
+            Command::Sub => {
+                write!(f, "sub")
+            },
+            Command::Neg => {
+                write!(f, "neg")
+            },
+            Command::Eq => {
+                write!(f, "eq")
+            },
+            Command::Gt => {
+                write!(f, "gt")
+            },
+            Command::Lt => {
+                write!(f, "lt")
+            },
+            Command::And => {
+                write!(f, "and")
+            },
+            Command::Or => {
+                write!(f, "or")
+            },
+            Command::Not => {
+                write!(f, "not")
+            },
+        }
+    }
+}
+
+pub struct VMWriter {
+    writer: BufWriter<File>,
+    filename: String,
+    function_name: String,
+    line_count: usize,
+    call_count: usize,
+}
+
+impl VMWriter {
+    pub fn new(f: File) -> Self {
+        VMWriter {
+            writer: BufWriter::<File>::new(f),
+            filename: String::new(),
+            function_name: String::new(),
+            line_count: 0,
+            call_count: 0,
+        }
+    }
+
+    pub fn write_push(&mut self, segment: Segment, index: i16) {
+        writeln!(self.writer, "push {} {}", segment.to_string(), index).unwrap();
+    }
+
+    pub fn write_pop(&mut self, segment: Segment, index: i16) {
+        writeln!(self.writer, "pop {} {}", segment.to_string(), index).unwrap();
+    }
+
+    pub fn write_arithmetic(&mut self, command: Command) {
+       writeln!(self.writer, "{}", command.to_string()).unwrap();
+    }
+
+    pub fn write_label(&mut self, label: &str) {
+        writeln!(self.writer, "label {}", label).unwrap();
+    }
+
+    pub fn write_goto(&mut self, label: &str) {
+        writeln!(self.writer, "goto {}", label).unwrap();
+    }
+
+    pub fn write_if(&mut self, label: &str) {
+        writeln!(self.writer, "if-goto {}", label).unwrap();
+    }
+
+    pub fn close(&mut self) {
+        self.writer.flush().unwrap();
+    }
+}
